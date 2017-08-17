9ad9438e55459951420716562d6ed87f87a831f3
Bjorn Inge <bjorninge@gmail.com>
2017-08-17 20:55:15 +0200

    fix nullpointer

--- NightscoutShareServer/Utils/Config.cs
+++ NightscoutShareServer/Utils/Config.cs
@@ -7,9 +7,9 @@ namespace NightscoutShareServer.Utils
 {
     public class Config
     {
-        
 
-        public static string NsHost => (Environment.GetEnvironmentVariable("NS_Host") ?? "").TrimEnd(new[] { '/' });
-        public static bool EnableMockedGlucoseMode => Environment.GetEnvironmentVariable("Enable_Mocked_Mode").ToLower() == "true";
+        public static string NsHost => "https://bjorningedia4.herokuapp.com";
+        //public static string NsHost => (Environment.GetEnvironmentVariable("NS_Host") ?? "").TrimEnd(new[] { '/' });
+        public static bool EnableMockedGlucoseMode => Environment.GetEnvironmentVariable("Enable_Mocked_Mode")?.ToLower() == "true";
     }
 }
