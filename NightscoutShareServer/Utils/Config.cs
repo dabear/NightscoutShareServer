266bca4a15249a60797fe3c5838971464d6eb8a8
Bjorn Inge <bjorninge@gmail.com>
2017-08-17 21:19:03 +0200

    nightscout integration check: show values in mmol and mgdl

--- NightscoutShareServer/Utils/Config.cs
+++ NightscoutShareServer/Utils/Config.cs
@@ -7,7 +7,7 @@ namespace NightscoutShareServer.Utils
 {
     public class Config
     {
-
+        
         public static string NsHost => (Environment.GetEnvironmentVariable("NS_Host") ?? "").TrimEnd(new[] { '/' });
         public static bool EnableMockedGlucoseMode => Environment.GetEnvironmentVariable("Enable_Mocked_Mode")?.ToLower() == "true";
     }
